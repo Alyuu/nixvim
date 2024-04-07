1dc88d50a19319cbb659b3217f02569d1cf58e5b
Alyu NixOS <alejandro.yusesc@gmail.com>
2024-04-08 00:54:55 +0200

    git signs

--- config/plugins/gitsigns.nix
+++ config/plugins/gitsigns.nix
@@ -1,5 +1,69 @@
 {
-	plugins.gitsigns = {
-		enable = true;
-	};
+    plugins.gitsigns = {
+        enable = true;
+        settings = {
+            signs = {
+                add = { 
+                    text = "+";
+                };
+                change = { 
+                    text = "~";
+                };
+                delete = { 
+                    text = "_";
+                };
+                topdelete = { 
+                    text = "‾";
+                };
+                changedelete = { 
+                    text = "~";
+                };
+            };
+        };
+    };
+
+    keymaps = [
+        {
+            action = "<CMD>Gitsigns stage_hunk<CR>";
+            key = "<leader>hs";
+            mode = "n";
+            options.desc = "Git [H]unk [S]tage";
+        }
+        {
+            action = "<CMD>Gitsigns reset_hunk<CR>";
+            key = "<leader>hr";
+            mode = "n";
+            options.desc = "Git [H]unk [R]eset";
+        }
+        {
+            action = "<CMD>Gitsigns stage_buffer<CR>";
+            key = "<leader>hS";
+            mode = "n";
+            options.desc = "Git stage buffer";
+        }
+        {
+            action = "<CMD>Gitsigns undo_stage_hunk<CR>";
+            key = "<leader>hu";
+            mode = "n";
+            options.desc = "Git [H]unk [U]ndo stage";
+        }
+        {
+            action = "<CMD>Gitsigns reset_buffer<CR>";
+            key = "<leader>hR";
+            mode = "n";
+            options.desc = "Git [H]unk [R]eset buffer";
+        }
+        {
+            action = "<CMD>Gitsigns preview_hunk<CR>";
+            key = "<leader>hp";
+            mode = "n";
+            options.desc = "Git [H]unk [P]review";
+        }
+        {
+            action = "<CMD>Gitsigns blame_line<CR>";
+            key = "<leader>hb";
+            mode = "n";
+            options.desc = "Git [B]lame";
+        }
+    ];
 }
