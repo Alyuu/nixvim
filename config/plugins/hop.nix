f90c083edc00909bd6765cf38c3c68c26927f214
Alejandro <100941712+Alyuu@users.noreply.github.com>
2024-04-10 14:22:20 +0200

    Update hop.nix

--- config/plugins/hop.nix
+++ config/plugins/hop.nix
@@ -7,21 +7,21 @@
           	{
                         key = "<leader><leader>s";
                         lua = true;
-        		action = "require'hop'.hint_char1()";
+        		action = "require'hop'.hint_char1";
         		mode = "n";
         		options.desc = "Hop [S]ingle char";
         	}
                 {
        		        key = "<leader><leader>/";
                         lua = true;
-        		action = "require'hop'.hint_patterns()";
+        		action = "require'hop'.hint_patterns";
         		mode = "n";
         		options.desc = "Hop search";
         	}
                 {
         		key = "<leader><leader>l";
                         lua = true;
-        		action = "require'hop'.hint_lines()";
+        		action = "require'hop'.hint_lines";
         		mode = "n";
         		options.desc = "Hop [L]ine";
         	}
