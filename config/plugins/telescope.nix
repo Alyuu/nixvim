e00c0b191537e196264b022b42a07c6d3aa7f1a4
Alejandro <100941712+Alyuu@users.noreply.github.com>
2024-04-10 13:49:28 +0200

    Update telescope.nix

--- config/plugins/telescope.nix
+++ config/plugins/telescope.nix
@@ -5,23 +5,23 @@
 		keymaps = {
 			"<leader>sf" = {
 				action = "find_files";
-				desc = "[S]earch [F]iles";
+				options.desc = "[S]earch [F]iles";
 			};
 			"<leader>sh" = {
 				action = "help_tags";
-				desc = "[S]earch [H]elp";
+				options.desc = "[S]earch [H]elp";
 			};
 			"<leader>sg" = {
 				action = "live_grep";
-				desc = "[S]earch by [G]rep";
+				options.desc = "[S]earch by [G]rep";
 			};
 			"<leader>sd" = {
 				action = "diagnostics";
-				desc = "[S]earch [D]iagnostics";
+				options.desc = "[S]earch [D]iagnostics";
 			};
 			"<leader>gf" = {
 				action = "git_files";
-				desc = "Search [G]it [F]iles";
+				options.desc = "Search [G]it [F]iles";
 			};
 		};
 		
