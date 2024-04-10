2346c06f9a9b3fbaa378f029a88ee1abd334a738
Alejandro <100941712+Alyuu@users.noreply.github.com>
2024-04-10 14:09:57 +0200

    Update hop.nix

--- config/plugins/hop.nix
+++ config/plugins/hop.nix
@@ -4,35 +4,26 @@
         };
 
         keymaps = [
+          	{
+                        lua = true;
+        		action = "require'hop'.hint_char1()";
+        		key = "<leader><leader>s";
+        		mode = "n";
+        		options.desc = "Hop [S]ingle char";
+        	}
                 {
-                        key = "<leader><leader>s";
-                        action.__raw = ''
-                        function()
-                          require'hop'.hint_char1()
-                        end
-                        '';
-                        options.remap = true;
-                        options.desc = "Hop [S]ingle char";
-                }
+                        lua = true;
+        		action = "require'hop'.hint_patterns()";
+        		key = "<leader><leader>/";
+        		mode = "n";
+        		options.desc = "Hop search";
+        	}
                 {
-                        key = "<leader><leader>/";
-                        action.__raw = ''
-                        function()
-                          require'hop'.hint_patterns()
-                        end
-                        '';
-                        options.remap = true;
-                        options.desc = "Hop search";
-                }
-                {
-                        key = "<leader><leader>l";
-                        action.__raw = ''
-                        function()
-                          require'hop'.hint_lines()
-                        end
-                        '';
-                        options.remap = true;
-                        options.desc = "Hop [L]ines";
-                }
+                        lua = true;
+        		action = "require'hop'.hint_lines()";
+        		key = "<leader><leader>l";
+        		mode = "n";
+        		options.desc = "Hop [L]ine";
+        	}
         ];
 }
