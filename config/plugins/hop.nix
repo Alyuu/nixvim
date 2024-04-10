d1313b5a11b4fd130492e31caefa844ccf8fa1a7
Alejandro <100941712+Alyuu@users.noreply.github.com>
2024-04-10 11:06:32 +0200

    Create hop.nix

---
+++ config/plugins/hop.nix
@@ -0,0 +1,26 @@
+{
+        plugins.hop = {
+                enable = true;
+        }
+
+        keymaps = [
+          	{
+        		action = ''require"hop".hint_char1()'';
+        		key = "<leader><leader>s";
+        		mode = "n";
+        		options.desc = "Hop [S]ingle char";
+        	}
+                {
+        		action = ''require"hop".hint_patterns()'';
+        		key = "<leader><leader>/";
+        		mode = "n";
+        		options.desc = "Hop search";
+        	}
+                {
+        		action = ''require"hop".hint_lines()'';
+        		key = "<leader><leader>l";
+        		mode = "n";
+        		options.desc = "Hop [L]ine";
+        	}
+        ];
+}
