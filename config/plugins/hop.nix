dd3d4ae43f26b3978aebd26f73aa2fb7b1499479
Alejandro <100941712+Alyuu@users.noreply.github.com>
2024-04-10 13:57:49 +0200

    Update hop.nix

--- config/plugins/hop.nix
+++ config/plugins/hop.nix
@@ -5,19 +5,19 @@
 
         keymaps = [
           	{
-        		action = ''require"hop".hint_char1()'';
+        		action = "require'hop'.hint_char1()";
         		key = "<leader><leader>s";
         		mode = "n";
         		options.desc = "Hop [S]ingle char";
         	}
                 {
-        		action = ''require"hop".hint_patterns()'';
+        		action = "require'hop'.hint_patterns()";
         		key = "<leader><leader>/";
         		mode = "n";
         		options.desc = "Hop search";
         	}
                 {
-        		action = ''require"hop".hint_lines()'';
+        		action = "require'hop'.hint_lines()";
         		key = "<leader><leader>l";
         		mode = "n";
         		options.desc = "Hop [L]ine";
