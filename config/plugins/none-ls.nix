4c1b7ac9972a4fc08eaa05b7391b0cfdca257750
Alyu NixOS <alejandro.yusesc@gmail.com>
2024-04-13 01:47:29 +0200

    added markdown diagnostics

--- config/plugins/none-ls.nix
+++ config/plugins/none-ls.nix
@@ -1,19 +1,20 @@
 {
-	plugins.none-ls = {
-		enable = true;
-		sources = {
-			diagnostics = {
-				stylelint.enable = true;
-				statix.enable = true;
-			};
-			formatting = {
-				csharpier.enable = true;
-				nixfmt.enable = true;
-				markdownlint.enable = true;
-				shellharden.enable = true;
-				shfmt.enable = true;
-				prettierd.enable = true;
-			};
-		};
-	};
+  plugins.none-ls = {
+    enable = true;
+    sources = {
+      diagnostics = {
+        stylelint.enable = true;
+        statix.enable = true;
+        markdownlint = true;
+      };
+      formatting = {
+        csharpier.enable = true;
+        nixfmt.enable = true;
+        markdownlint.enable = true;
+        shellharden.enable = true;
+        shfmt.enable = true;
+        prettierd.enable = true;
+      };
+    };
+  };
 }
