450cd426702dc29d55b2d3713b046ace9fb98a74
Alejandro <100941712+Alyuu@users.noreply.github.com>
2024-04-12 13:14:10 +0200

    Update trouble.nix

--- config/plugins/trouble.nix
+++ config/plugins/trouble.nix
@@ -1,5 +1,39 @@
 {
         plugins.trouble = {
                 enable = true;
+                icons = false;
+                fold_open = "v";
+                fold_closed = ">";
+                indent_lines = false;
+                signs = {
+                        error = "error";
+                        warning = "warn";
+                        hint = "hint";
+                        information = "info";
+                };
         };
+
+        keymaps = [
+          	{
+                        key = "<leader>tt";
+                        lua = true;
+        		action = "require'trouble'.toggle";
+        		mode = "n";
+        		options.desc = "[T]oggle [T]rouble";
+        	}
+                {
+       		        key = "[t";
+                        lua = true;
+        		action = "require("trouble").next({skip_groups = true, jump = true})";
+        		mode = "n";
+        		options.desc = "Next [T]rouble";
+        	}
+                {
+       		        key = "]t";
+                        lua = true;
+        		action = "require("trouble").previous({skip_groups = true, jump = true})";
+        		mode = "n";
+        		options.desc = "Previous [T]rouble";
+        	}
+        ];
 }
