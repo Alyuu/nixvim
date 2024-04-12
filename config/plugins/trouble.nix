cb43ed72b6d4deba7ee8cff55d8f0b84bd2c701a
workLaptop <alejandro.yusesc@gmail.com>
2024-04-12 12:35:16 +0000

    Trouble nvim

--- config/plugins/trouble.nix
+++ config/plugins/trouble.nix
@@ -1,41 +1,45 @@
 {
-        plugins.trouble = {
-                enable = true;
-                settings = {
-                        icons = false;
-                        fold_open = "v";
-                        fold_closed = ">";
-                        indent_lines = false;
-                        signs = {
-                                error = "error";
-                                warning = "warn";
-                                hint = "hint";
-                                information = "info";
-                        };
+    plugins.trouble = {
+        enable = true;
+        settings = {
+            indent_lines = false;
+            fold_open = "";
+            fold_closed = "";
+            signs = {
+                signs = {
+                    error = "";
+                    warning = "";
+                    hint = "";
+                    information = "";
+                    other = "";
                 };
+            };
         };
+    };
 
-        keymaps = [
-          	{
-                        key = "<leader>tt";
-                        lua = true;
-        		action = "require'trouble'.toggle";
-        		mode = "n";
-        		options.desc = "[T]oggle [T]rouble";
-        	}
-                {
-       		        key = "[t";
-                        lua = true;
-        		action = "function() require'trouble'.next({skip_groups = true, jump = true}) end";
-        		mode = "n";
-        		options.desc = "Next [T]rouble";
-        	}
-                {
-       		        key = "]t";
-                        lua = true;
-        		action = "function() require'trouble'.previous({skip_groups = true, jump = true}) end";
-        		mode = "n";
-        		options.desc = "Previous [T]rouble";
-        	}
-        ];
+    keymaps = [
+        {
+            key = "<leader>tt";
+            lua = true;
+            action = "require'trouble'.toggle";
+            mode = "n";
+            options.desc = "[T]oggle [T]rouble";
+        }
+        {
+            key = "[t";
+            lua = true;
+            action =
+                "function() require'trouble'.next({skip_groups = true, jump = true}) end";
+            mode = "n";
+            options.desc = "Next [T]rouble";
+        }
+        {
+            key = "]t";
+            lua = true;
+            action =
+                "function() require'trouble'.previous({skip_groups = true, jump = true}) end";
+            mode = "n";
+            options.desc = "Previous [T]rouble";
+        }
+    ];
 }
