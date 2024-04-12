d16b49f48b035fa10821df078a616ccfe11af6f0
Alejandro <100941712+Alyuu@users.noreply.github.com>
2024-04-12 13:29:19 +0200

    Update trouble.nix

--- config/plugins/trouble.nix
+++ config/plugins/trouble.nix
@@ -26,14 +26,14 @@
                 {
        		        key = "[t";
                         lua = true;
-        		action = "require'trouble'.next({skip_groups = true, jump = true})";
+        		action = "function() require'trouble'.next({skip_groups = true, jump = true}) end";
         		mode = "n";
         		options.desc = "Next [T]rouble";
         	}
                 {
        		        key = "]t";
                         lua = true;
-        		action = "require'trouble'.previous({skip_groups = true, jump = true})";
+        		action = "function() require'trouble'.previous({skip_groups = true, jump = true}) end";
         		mode = "n";
         		options.desc = "Previous [T]rouble";
         	}
