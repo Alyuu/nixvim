12d55b2e4d9e1cad76bb7888e7a82bbbecd74c8c
Alejandro <100941712+Alyuu@users.noreply.github.com>
2024-04-12 13:16:26 +0200

    Update hop.nix

--- config/plugins/hop.nix
+++ config/plugins/hop.nix
@@ -8,21 +8,30 @@
                         key = "<leader><leader>s";
                         lua = true;
         		action = "require'hop'.hint_char1";
-        		mode = "n";
+        		mode = [
+                                "n"
+                                "v"
+                        ];
         		options.desc = "Hop [S]ingle char";
         	}
                 {
        		        key = "<leader><leader>/";
                         lua = true;
         		action = "require'hop'.hint_patterns";
-        		mode = "n";
+        		mode = [
+                                "n"
+                                "v"
+                        ];
         		options.desc = "Hop search";
         	}
                 {
         		key = "<leader><leader>l";
                         lua = true;
         		action = "require'hop'.hint_lines";
-        		mode = "n";
+        		mode = [
+                                "n"
+                                "v"
+                        ];
         		options.desc = "Hop [L]ine";
         	}
         ];
