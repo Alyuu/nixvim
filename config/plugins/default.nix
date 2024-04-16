a0d7b5530748db748816c132d12364afb2c5342f
Alyu NixOS <alejandro.yusesc@gmail.com>
2024-04-16 16:40:06 +0200

    why do i need linting and formatting?

--- config/plugins/default.nix
+++ config/plugins/default.nix
@@ -1,21 +1,21 @@
 {
     imports = [
         ./oil.nix
-            ./lualine.nix
-            ./telescope.nix
-            ./treesitter.nix
-            ./lsp.nix
-            ./cmp.nix
-            ./comment.nix
-            ./colorizer.nix
-            ./neogit.nix
-            ./gitsigns.nix
-            ./hop.nix
-            ./fidget.nix
-            ./trouble.nix
-            ./indent-blankline.nix
-            ./surround.nix
-            ./conform.nix
-            ./lint.nix
+        ./lualine.nix
+        ./telescope.nix
+        ./treesitter.nix
+        ./lsp.nix
+        ./cmp.nix
+        ./comment.nix
+        ./colorizer.nix
+        ./neogit.nix
+        ./gitsigns.nix
+        ./hop.nix
+        ./fidget.nix
+        ./trouble.nix
+        ./indent-blankline.nix
+        ./surround.nix
+        # ./conform.nix
+        # ./lint.nix
     ];
 }
