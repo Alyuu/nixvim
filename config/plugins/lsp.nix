a9dfc756dfcb8fee75d7628e832dcec9e2ecddfd
Alyu NixOS <alejandro.yusesc@gmail.com>
2024-08-24 14:03:17 +0200

    error ruff lsp

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -5,7 +5,6 @@
             servers = {
                 bashls.enable = true;
                 gopls.enable = true;
-                ruff-lsp.enable = true;
                 csharp-ls.enable = true;
                 cssls.enable = true;
                 html.enable = true;
